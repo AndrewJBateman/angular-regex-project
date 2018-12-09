a51a6ba5161a4aae0f6d3a1227400e3993f374b7
Andrew Bateman <gomezbateman@yahoo.com>
2018-12-09 11:51:06 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-regex-project';
+
+    private content: string;
+    public query: string;
+
+    public constructor() {
+        this.content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent a quam ornare, bibendum ligula a, rhoncus ligula. Etiam aliquet, justo sollicitudin imperdiet luctus, nulla justo sodales mi, sit amet semper nisl velit vel massa. In hac habitasse platea dictumst.";
+    }
+
+    public highlight() {
+        if(!this.query) {
+            return this.content;
+        }
+        /*The regular expression looks for all case insensitive occurrences of this.query.
+        Take the matches and wrap them in HTML tags with the CSS class name that we created.*/
+        return this.content.replace(new RegExp(this.query, "gi"), match => {
+            return '<span class="highlightText">' + match + '</span>';
+        });
+    }
+
 }
